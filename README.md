d78e4c79c5f982c7fd6381c65fec414b198af53e
BnjMui <jonasae@hiof.no>
2024-09-03 00:46:31 +0200

    Fullført oppgaven

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# portfolio
+# portfolio_v1
 
-Har i all forstand gått ut og forbi wireFramen jeg opprinnelig lagde her.
+OBS(Last sequenceDiagram.png er forsåvidt ikke en del av kravene til oppgaven. Henvender til de to andre .png-filene i mappen for oppgave 1.)
+
+Jeg startet oppgaven med å lage en wireframe, to sekvensdiagram og en .json fil med et eksempel på hvordan jeg tenkte at dataen skulle se ut.
+
+Etter dette gikk jeg videre til html, css og javascript.
+I js lagde jeg først funksjonalitet for å hente data fra .json filen, som jeg kopierte inn i application-mappen, som inneholder selve applikasjonen. Deretter lagde jeg funksjonalitet for å lagre dataen fra form i html, men lagret dette i en midlertidig liste i js-filen. exampleData.json ble også modifisert for å passe bedre inn i prosjektet, men hadde et godt utgangspunkt.
+
+Til sist satt jeg opp hono-serveren. Dette gjorde jeg ved hjelp av eksempler fra kursets repository, filene fra timen vi hadde i forrige uke, og dokumentasjon på nett. Kilde til dette er: https://hono.dev/docs/getting-started/basic
+
+Jeg endte opp med å bruke node:fs for å hente og skrive data til .json filen. Dokumentasjonen jeg har brukt til dette: https://www.geeksforgeeks.org/how-to-add-data-in-json-file-using-node-js/
+
+Grunnen til at wireframe og nettside ikke samsvarer helt er at jeg har i ettertid utviklet designet videre fra wireframen jeg i utgangspunktet lagde.
+
+Helt til slutt valgte jeg å lage et nytt sekvensdiagram med kunnskapen jeg nå har fått. Jeg ble aldri helt fornøyd med de to første sekvensdiagrammene, men det var det jeg fikk til å lage med nåværende kunnskap.
+
+I denne oppgaven valgte jeg å gjøre det mulig for serveren å skrive til og hente fra json filen exampleData.json, ettersom jeg selv mener dette er relevant for kurset, og ønsker meg så mye kunnskap som mulig.
+
+I et mer realistisk scenario, eller kanskje videre utvikling av denne applikasjonen, planlegger jeg å legge til en remove/change-funksjon, i tilfelle man legger til feil data, eller ønsker å fjerne et prosjekt fra applikasjonen.
--- application/main.js
+++ application/main.js
@@ -1,29 +1,40 @@
 import "./static/style.scss";
 //Bruker fetch for å hente data fra json filen.
-const getJson = async () => {
+// const getJson = async () => {
+//   try {
+//     const response = await fetch("./exampleData.json");
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+//   } catch {
+//     console.log(console.error("error"));
+//   }
+// };
+// const data = await getJson();
+
+//Funksjon for å hente data fra hono-server.
+const getProjects = async () => {
   try {
-    const response = await fetch("./exampleData.json");
+    const response = await fetch("http://localhost:3999");
     const data = await response.json();
-    console.log(data);
     return data;
   } catch {
     console.log(console.error("error"));
   }
 };
-const data = await getJson();
-
 //Funksjon for å vise dataen i html.
 const projectList = document.getElementById("project-list");
 
-const renderList = () => {
+const renderList = async () => {
   let projectListHTML = "";
+  const data = await getProjects();
 
   for (const p of data) {
     projectListHTML += `
       <li>
       <article>
       <h3>${p.projectName}</h3>
-      <p>${p.category}</p><p>Link to project: <a href="${p.url}">${p.url}</a></p>
+      <p>${p.category}</p><p>Link to project: <a target="_blank" href="${p.url}">${p.url}</a></p>
       <p>Created at: ${p.date}</p>
       <a href="#">Read more</a>
       </article>
@@ -35,23 +46,37 @@ renderList();
 
 //Funksjon for å legge til nye prosjekter.
 let tempArray = [];
-
+//Oppdaterer denne for å kunne pushe data inn i json filen via hono-server.
 const msg = document.getElementById("success-message");
-const submit = (e) => {
+const submit = async (e) => {
   e.preventDefault();
   const form = e.target;
-  const projectName = form.name.value;
-  const projectUrl = form.link.value;
-  const projectCat = form.category.value;
-  const projectDescription = form.description.value;
-  tempArray.push({
-    projectName: projectName,
-    url: projectUrl,
-    category: projectCat,
-    description: projectDescription,
+  const newProject = {
+    projectName: form.name.value,
+    url: form.link.value,
+    category: form.category.value,
+    description: form.description.value,
     date: new Date().toISOString().split("T")[0], //date funksjon hentet fra chatgpt
-  });
-  console.log(tempArray);
+  };
+
+  try {
+    const response = await fetch("http://localhost:3999/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProject),
+    });
+    if (response.status === 201) {
+      console.log("Project added to server data");
+      console.log("ProjectData: \n", newProject);
+      renderList();
+    } else {
+      console.error("Error when adding new project to server");
+    }
+  } catch {
+    console.log(console.error("error", error));
+  }
   form.reset();
   msg.textContent = "Your project was succesfully added!";
   msg.style.color = "green";
